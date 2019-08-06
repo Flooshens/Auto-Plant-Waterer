b54588f9f0f72634e635e0f9ccb3f366bf5cb16e
Flooshens <53634236+Flooshens@users.noreply.github.com>
2019-08-05 20:16:29 -0500

    Update main.py

--- Esp8266#2/main.py
+++ Esp8266#2/main.py
@@ -1,33 +1,26 @@
-# Write your code here :-)
 # Complete project details at https://RandomNerdTutorials.com
 
-
 def sub_cb(topic, msg):
-    print((topic, msg))
-
+  print((topic, msg))
 
 def connect_and_subscribe():
-    global client_id, mqtt_server, topic_sub
-    client = MQTTClient(client_id, mqtt_server)
-    client.set_callback(sub_cb)
-    client.connect()
-    client.subscribe(topic_sub)
-    print(
-        "Connected to %s MQTT broker, subscribed to %s topic" % (mqtt_server, topic_sub)
-    )
-    return client
-
+  global client_id, mqtt_server, topic_sub
+  client = MQTTClient(client_id, mqtt_server)
+  client.set_callback(sub_cb)
+  client.connect()
+  client.subscribe(topic_sub)
+  print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
+  return client
 
 def restart_and_reconnect():
-    print("Failed to connect to MQTT broker. Reconnecting...")
-    time.sleep(10)
-    machine.reset()
-
+  print('Failed to connect to MQTT broker. Reconnecting...')
+  time.sleep(10)
+  machine.reset()
 
 try:
-    client = connect_and_subscribe()
+  client = connect_and_subscribe()
 except OSError as e:
-    restart_and_reconnect()
+  restart_and_reconnect()
 
 while True:
   try:
